13fa8e7c43a9941c35e6ec43bd6e7b5d70047566
samcolson4 <samcolson4@gmail.com>
2020-10-12 15:20:00 +0100

    Split testing files

--- spec/note-modelSpec.js
+++ spec/note-modelSpec.js
@@ -12,14 +12,14 @@ class testingSuite {
 
     assert.isTrue(text, app.text == "I have no idea what I'm doing")
   }
-  
+
 }
 
 
 class assert {
   static isTrue(text, assertionToCheck) {
       if (!assertionToCheck) {
-        throw new Error("Assertion failed: " + text + " is not truthy");
+        throw new Error("%c Assertion failed: " + text + " is not truthy", 'background-color: floralwhite; color: red');
       } 
       else {
         console.log("%c Smashed it: " + text, 'background-color: floralwhite; color: green')
