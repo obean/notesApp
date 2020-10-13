5b601f916c73fe3ec0c6565e8b7706bca4064be9
ollie beney <ombeney@gmail.com>
2020-10-13 10:04:26 +0100

    changed incorrect assertions to show red message rather than throw errors, just for cleanliness

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -2,7 +2,7 @@ class assert {
 
   static isTrue(text, assertionToCheck) {
       if (!assertionToCheck) {
-        throw new Error("%c Assertion failed: " + text + " is not truthy", 'background-color: floralwhite; color: red');
+        console.log("%c Assertion failed: " + text + " is not truthy", 'background-color: floralwhite; color: red');
       } 
       else {
         console.log("%c Smashed it: " + text, 'background-color: floralwhite; color: green')
