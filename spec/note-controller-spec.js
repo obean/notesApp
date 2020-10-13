b3c8e5052f284284d8cbd202292c52f1c122782d
Connor Worthington <worthington.connor@gmail.com>
2020-10-13 15:26:06 +0100

    adds getHtml and insertHtml to controller

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -9,6 +9,13 @@ class noteControllerSpec {
     assert.isTrue("checking it instantiates with a list model", controller.listmodel.constructor.name === listModel.constructor.name)
   }
 
+  static instantiatesWithnoteListView() {
+    var noteView = new noteListView
+    var listModel = new noteListModel
+    var controller = new noteController(listModel)
+    assert.isTrue("it instantiates with note-list-view", controller.noteview.constructor.name === noteView.constructor.name)
+  }
+
   static addsANote() {
     var listModel = new noteListModel
     var controller = new noteController(listModel)
@@ -16,5 +23,7 @@ class noteControllerSpec {
   }
 }
 
+
 noteControllerSpec.instantiatesWithnoteApp()
 noteControllerSpec.addsANote()
+noteControllerSpec.instantiatesWithnoteListView()
--- src/note-controller.js
+++ src/note-controller.js
@@ -5,5 +5,21 @@ class noteController {
   constructor(listModel) {
     this.listmodel = listModel
     this.listmodel.addNote("Favourite drink: seltzer")
+    this.noteview = new noteListView(this.listmodel)
   }
+
+  getHtml() {
+    return this.noteview.output();
+  }
+
+  insertHtml() {
+    var noteHtml = document.getElementById('app')
+    console.log(noteHtml)
+    noteHtml.innerHTML = this.getHtml();
+  }
+
 }
+
+var listModel = new noteListModel
+var controller = new noteController(listModel)
+controller.insertHtml();
